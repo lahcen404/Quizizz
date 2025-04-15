1a18214bc59030129df5cbb1f8cd972fa0c8770d
lahcen404 <lahcen.maskour2003@gmail.com>
2025-04-15 17:52:55 +0100

    Update in Home - Add Number of Questions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./shared/navbar/navbar.component";
 import { FooterComponent } from "./shared/footer/footer.component";
 import { HomeComponent } from "./home/home.component";
+import { HistoryComponent } from "./history/history.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, HomeComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, HomeComponent, HistoryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';  
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-history',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './history.component.html',
   styleUrl: './history.component.css'
 })
 export class HistoryComponent {
+httpClient = inject(HttpClient);
+public data: Array<any> = [];
 
+ngOnInit() {
+  this.httpClient.get('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy').subscribe({
+    next: (data: any) => {
+      console.log(data);
+      this.data = data;
+    }, error: (err) => console.log(err)
+  });
+}
 }
