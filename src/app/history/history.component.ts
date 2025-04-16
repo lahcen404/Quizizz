254aa5848f51363553e9683fa6aeb8714a692892
lahcen404 <lahcen.maskour2003@gmail.com>
2025-04-16 12:49:21 +0100

    Fetch data from API in Home Page (Form)

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -9,15 +9,5 @@ import { Component, inject } from '@angular/core';
   styleUrl: './history.component.css'
 })
 export class HistoryComponent {
-httpClient = inject(HttpClient);
-public data: Array<any> = [];
 
-ngOnInit() {
-  this.httpClient.get('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy').subscribe({
-    next: (data: any) => {
-      console.log(data);
-      this.data = data;
-    }, error: (err) => console.log(err)
-  });
-}
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  private http = inject(HttpClient);
 
+  categories: Array<any> = [];
+  // par default 
+  selectedCategory: number = 9; 
+  selectedDifficulty: string = 'medium';
+  numQuestions: number = 10;
+  httpClient: any;
+  quizData: any;
+
+  ngOnInit(): void {
+    this.fetchCategories();
+  }
+
+  fetchCategories(): void {
+    this.http.get('https://opentdb.com/api_category.php').subscribe({
+      next: (res: any) => {
+        this.categories = res.trivia_categories;
+      },
+      error: (err) => {
+        console.error('Error fetching categories:', err);
+      }
+    });
+  }
+
+  startQuiz(): void {
+    console.log('Quiz settings:', {
+      category: this.selectedCategory,
+      difficulty: this.selectedDifficulty,
+      numQuestions: this.numQuestions
+    });
+
+    const url = `https://opentdb.com/api.php?amount=${this.numQuestions}&category=${this.selectedCategory}&difficulty=${this.selectedDifficulty}&type=multiple`;
+
+    this.http.get<any>(url).subscribe({
+      next: (res) => {
+        console.log('api url:', url);
+        console.log('quiiz Data:', res);
+        this.quizData = res.results;
+      },
+      error: (err) => {
+        console.error('api eerror:', err);
+      }
+    });
+  }
 }
