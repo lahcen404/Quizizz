b1e5f24b08dd486582222c38da396b3cb2525806
lahcen404 <lahcen.maskour2003@gmail.com>
2025-04-19 23:39:53 +0100

    Display CAtegory name in Result not Number

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -13,6 +13,35 @@ import { Score } from '../shared/score.model';
   styleUrls: ['./result.component.css']
 })
 export class ResultComponent implements OnInit {
+
+  categoryMap: { [key: string]: string } = {
+    '9': 'General Knowledge',
+    '10': 'Entertainment: Books',
+    '11': 'Entertainment: Film',
+    '12': 'Entertainment: Music',
+    '13': 'Entertainment: Musicals & Theatres',
+    '14': 'Entertainment: Television',
+    '15': 'Entertainment: Video Games',
+    '16': 'Entertainment: Board Games',
+    '17': 'Science & Nature',
+    '18': 'Science: Computers',
+    '19': 'Science: Mathematics',
+    '20': 'Mythology',
+    '21': 'Sports',
+    '22': 'Geography',
+    '23': 'History',
+    '24': 'Politics',
+    '25': 'Art',
+    '26': 'Celebrities',
+    '27': 'Animals',
+    '28': 'Vehicles',
+    '29': 'Entertainment: Comics',
+    '30': 'Science: Gadgets',
+    '31': 'Entertainment: Japanese Anime & Manga',
+    '32': 'Entertainment: Cartoon & Animations'
+  };
+  
+
   private route = inject(ActivatedRoute);
   private router = inject(Router);
   // private storageService = inject(StorageService);
@@ -29,7 +58,8 @@ export class ResultComponent implements OnInit {
     this.route.queryParams.subscribe(params => {
       this.score = +params['score'] || 0;
       this.totalQuestions = +params['totalQuestions'] ;
-      this.category = params['category'] || 'Unknown';
+       const categoryId = params['category'];
+      this.category = this.categoryMap[categoryId] || 'Unknown';
       this.difficulty = params['difficulty'] || 'Unknown';
       this.timed = params['timed'] === 'true';
     });
